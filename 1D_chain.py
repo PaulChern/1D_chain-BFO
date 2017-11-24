bf084b7b19f88a38d7745ee3d9be9cffdb84708f
PaulChern <cpiphy2011@gmail.com>
2017-11-24 14:54:05 +0800

    if the initial strain is too large, the energy will not be bounded

--- 1D_chain.py
+++ 1D_chain.py
@@ -34,7 +34,7 @@ DW_size = 102
 a_1 = 4.9*(T - 1103)*1E5*1E-10
 a_11 = 5.42E8*1E-10
 a_12 = 1.5E8*1E-10
-G_11 = 0.6*0.98*1E10*1E-10
+G_11 = 0.6*0.98*1E-10*1E-10
 
 c_1111 = 3.02E11*1E-10
 c_1122 = 1.62E11*1E-10
@@ -47,9 +47,9 @@ q_1111 = 2*c_1111*Q_1111
 q_1122 = 2*c_1111*Q_1122
 q_1212 = 2*c_1212*Q_1212
 
-Px = np.array([ x for x, _ in zip(sp.numbered_symbols('Px'), range(0,DW_size))])
-Py = np.array([ x for x, _ in zip(sp.numbered_symbols('Py'), range(0,DW_size))])
-Pz = np.array([ x for x, _ in zip(sp.numbered_symbols('Pz'), range(0,DW_size))])
+Px = np.array([ x for x, _ in zip(sp.numbered_symbols('Px_'), range(0,DW_size))])
+Py = np.array([ x for x, _ in zip(sp.numbered_symbols('Py_'), range(0,DW_size))])
+Pz = np.array([ x for x, _ in zip(sp.numbered_symbols('Pz_'), range(0,DW_size))])
 #Px[0] = -1; Px[101] = 1; Py[0] = -1; Py[101] = -1; Pz[0] = -1; Pz[101] = -1
 
 P = np.array([Px, Py, Pz])
@@ -59,23 +59,23 @@ Py_initial = np.array(DW_size*[-1])
 Pz_initial = np.array(DW_size*[-1])
 P_initial = np.array([Px_initial, Py_initial, Pz_initial])
 
-epsilon_11 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_11'), range(0,DW_size))])
-epsilon_22 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_22'), range(0,DW_size))])
-epsilon_33 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_33'), range(0,DW_size))])
-epsilon_12 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_12'), range(0,DW_size))])
-epsilon_23 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_23'), range(0,DW_size))])
-epsilon_31 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_31'), range(0,DW_size))])
+epsilon_11 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_11_'), range(0,DW_size))])
+epsilon_22 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_22_'), range(0,DW_size))])
+epsilon_33 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_33_'), range(0,DW_size))])
+epsilon_12 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_12_'), range(0,DW_size))])
+epsilon_23 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_23_'), range(0,DW_size))])
+epsilon_31 = np.array([ x for x, _ in zip(sp.numbered_symbols('epsilon_31_'), range(0,DW_size))])
 epsilon = np.array([epsilon_11, epsilon_22, epsilon_33, epsilon_12, epsilon_23, epsilon_31])
 
 variables_initial = [Px_initial,
                      Py_initial,
                      Pz_initial,
-                     np.random.rand(1,DW_size)[0]/10.0,
-                     np.random.rand(1,DW_size)[0]/10.0,
-                     np.random.rand(1,DW_size)[0]/10.0,
-                     np.random.rand(1,DW_size)[0]/10.0,
-                     np.random.rand(1,DW_size)[0]/10.0,
-                     np.random.rand(1,DW_size)[0]/10.0]
+                     np.random.rand(1,DW_size)[0]/100.0,
+                     np.random.rand(1,DW_size)[0]/100.0,
+                     np.random.rand(1,DW_size)[0]/100.0,
+                     np.random.rand(1,DW_size)[0]/100.0,
+                     np.random.rand(1,DW_size)[0]/100.0,
+                     np.random.rand(1,DW_size)[0]/100.0]
 
 variables = to_variables(P) + to_variables(epsilon)
 
@@ -90,6 +90,7 @@ F_landau_sym = np.sum(a_1*(P[0]**2 + P[1]**2 + P[2]**2)
                     + q_1122*(epsilon_11*P[1]**2 + epsilon_22*P[2]**2 + epsilon_33*P[0]**2)
                     + 0.5*q_1212*(epsilon_12*P[0]*P[1] + epsilon_23*P[1]*P[2] + epsilon_31*P[2]*P[0]))
 
+print(F_landau_sym)
 F_landau_num = sp.lambdify(variables, F_landau_sym, modules='numpy')
 
 cons = [{'type': 'eq', 'fun': lambda x:  (to_tensor(np.array(x))[0,0] + 1)**2},
